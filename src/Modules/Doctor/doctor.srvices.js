d4d82bae6d2517de2970224003676ac93a60c44c
abdullahrabi3 <alhdydy216@gmail.com>
2025-06-23 10:21:10 +0300

    first commit

--- src/Modules/Doctor/doctor.srvices.js
+++ src/Modules/Doctor/doctor.srvices.js
@@ -1,4 +1,5 @@
 import DoctorModel from "../../DB/models/doctor.models.js";
+import UserModel from "../../DB/models/user.models.js";
 
 // الدكتور يقوم بالحصول على المرضى المشتركين في حسابه
 export const getSubscribedUsers = async (req, res, next) => {
@@ -9,10 +10,15 @@ export const getSubscribedUsers = async (req, res, next) => {
 
   if (!doctor) return next(new Error("Doctor not found", { cause: 404 }));
 
+  const totalUsers = await UserModel.countDocuments(); // كل المرضى في السيستم
+  const subscribedCount = doctor.patients.length; // عدد المرضى المشتركين مع الدكتور
+
   return res.status(200).json({
     key: true,
     code: 200,
     message: "Subscribed users fetched",
-    data: doctor.patients,
+    totalUsers,
+    subscribedUsers: subscribedCount,
+    patients: doctor.patients, // تفاصيل المرضى المشتركين
   });
 };
